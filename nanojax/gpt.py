fixing bug in step count in train, nesterov calculation in momentum, making rope impl. clearer

nanojax/gpt.py
@@ -43,7 +43,7 @@ def apply_rope(x: jax.Array, cos: jax.Array, sin: jax.Array) -> jax.Array:
     y1 = x1 * cos - x2 * sin
     y2 = x1 * sin + x2 * cos
     # stitch our embedding vector back up
-    return jnp.append(y1, y2, axis=-1)
+    return jnp.concatenate([y1, y2], axis=-1)
 
 
 @dataclass

nanojax/muon.py
@@ -65,7 +65,7 @@ class Muon:
         momentum = (1 - frac) * 0.85 + frac * 0.95
         mu = jax.tree.map(lambda m, g: m * momentum + (1 - momentum) * g, self.mu, grads.h)
         # nesterov update
-        v = jax.tree.map(lambda m, g: g * (1 - momentum) + momentum * g, mu, grads.h)
+        v = jax.tree.map(lambda m, g: g * (1 - momentum) + momentum * m, mu, grads.h)
 
         def newton_shulz(G: jax.Array, ns_steps: int):
             a, b, c = (3.4445, -4.7750,  2.0315)
@@ -86,7 +86,7 @@ class Muon:
             G = G.astype(jnp.float32)
             return G
 
-        muon_update = jax.tree.map(lambda u, g: newton_shulz(u, self.ns_steps) * (max(1, g.shape[-2] / g.shape[-1]))**0.5, v, grads.h)
+        muon_update = jax.tree.map(lambda u, g: newton_shulz(u, self.ns_steps) * (jnp.maximum(1, g.shape[-2] / g.shape[-1]))**0.5, v, grads.h)
 
         updates = GPT(
             wte=adamw_update[0],

scripts/base_train.py
@@ -87,7 +87,6 @@ while True:
     x, y = next(train_loader)
     log_dict = {"loss": float(loss)}
     print(f"Step {step}/{num_steps} | Loss: {loss:.3f} / {expected_loss:.3f} ")
-    step += 1 
     if step % 10 == 0:
         # log profiling every now and then
         jax.block_until_ready(loss)
@@ -107,6 +106,7 @@ while True:
                 idx = jnp.concat([idx, pred], axis=1)
             print(tokenizer.decode(idx[0]))
 
+    step += 1 
     trackio.log(log_dict)
     if step == num_steps:
         break