adopt modded-nanogpt techniques: zero init for projections/lm_head, relu², logit softcapping, fan-in-fan-out weight init. bumped muon lrs and fixed missing lr scaling on muon updates. fixed recompilations in eval.py

nanojax/gpt.py
@@ -104,31 +104,36 @@ class GPT:
         # random state must be explicitly managed in JAX by "splitting"
         # random keys. fold_in_str does this by splitting the base key
         # based on the hash of a given string - in this case the weight name.
-        std = 0.02
-        wte = jax.random.normal(fold_in_str(rng, "wte"), (cfg.vocab_size, cfg.n_embed)) * std
+        # mean 0, std 1 initialization for embedding layer
+        def init_linear(rng, shape):
+            # fan-in-fan-out initialization https://arxiv.org/pdf/2310.17813
+            fan_in, fan_out = shape
+            std = 1.0 / math.sqrt(fan_in) * min(1.0, math.sqrt(fan_out / fan_in))
+            return jax.random.normal(rng, shape) * std
+
+        wte = jax.random.normal(fold_in_str(rng, "wte"), (cfg.vocab_size, cfg.n_embed)) 
         h = []
+        head_dim = cfg.n_embed // cfg.n_head
         for i in range(cfg.n_layer):
-            # regular mean-zero, std 0.02 weight initialisation
-            # TODO - why does nanochat do some funky initialiation?
-            head_dim = cfg.n_embed // cfg.n_head
-            resid_std = std / math.sqrt(2 * cfg.n_layer)
-    
+            # modded-nanogpt suggestion: zero out c_proj in attn and mlp layers
             attn = Attention(
-                c_q=jax.random.normal(fold_in_str(rng, f"{i}_c_q"), (cfg.n_embed, cfg.n_head * head_dim)) * std,
-                c_k=jax.random.normal(fold_in_str(rng, f"{i}_k"), (cfg.n_embed, cfg.n_kv_head * head_dim)) * std,
-                c_v=jax.random.normal(fold_in_str(rng, f"{i}_v"), (cfg.n_embed, cfg.n_kv_head * head_dim)) * std,
-                c_proj=jax.random.normal(fold_in_str(rng, f"{i}_o"), (cfg.n_embed, cfg.n_embed)) * resid_std,
+                c_q=init_linear(fold_in_str(rng, f"{i}_c_q"), (cfg.n_embed, cfg.n_head * head_dim)),
+                c_k=init_linear(fold_in_str(rng, f"{i}_k"), (cfg.n_embed, cfg.n_kv_head * head_dim)),
+                c_v=init_linear(fold_in_str(rng, f"{i}_v"), (cfg.n_embed, cfg.n_kv_head * head_dim)),
+                # c_proj=init_linear(fold_in_str(rng, f"{i}_o"), (cfg.n_embed, cfg.n_embed)) * resid_std,
+                c_proj = jnp.zeros((cfg.n_embed, cfg.n_embed))
             )
     
             mlp = MLP(
-                c_fc=jax.random.normal(fold_in_str(rng, f"{i}_c_fc"), (cfg.n_embed, 4 * cfg.n_embed)) * std,
-                c_proj=jax.random.normal(fold_in_str(rng, f"{i}_c_proj"), (4 * cfg.n_embed, cfg.n_embed)) * resid_std,
+                c_fc=init_linear(fold_in_str(rng, f"{i}_c_fc"), (cfg.n_embed, 4 * cfg.n_embed)),
+                # c_proj=init_linear(fold_in_str(rng, f"{i}_c_proj"), (4 * cfg.n_embed, cfg.n_embed)) * resid_std,
+                c_proj=jnp.zeros((4 * cfg.n_embed, cfg.n_embed))
             )
             h.append(Block(attn=attn, mlp=mlp))
 
-        # TODO - why does nanochat zero out classifier weights and c_proj in mlps?
-        # ANSWER: see modded-nanogpt
-        lm_head = jax.random.normal(fold_in_str(rng, "lm_head"), (cfg.n_embed, cfg.vocab_size)) * std
+        # modded-nanogpt suggestion: zero out classifier weights
+        # lm_head = jax.random.normal(fold_in_str(rng, "lm_head"), (cfg.n_embed, cfg.vocab_size)) * std
+        lm_head = jnp.zeros((cfg.n_embed, cfg.vocab_size))
         return GPT(
             wte=wte,
             h=h,
@@ -198,13 +203,16 @@ class GPT:
             ### mlp
             mlp_in = rms_norm(x)
             mlp_out = jnp.einsum("bse,eE->bsE", mlp_in, mlp.c_fc.astype(compute_dtype))
-            mlp_out = jax.nn.gelu(mlp_out) # TODO why does nanochat use relu^2? ANSWER: see modded-nanogpt
+            mlp_out = jax.lax.square(jax.nn.relu(mlp_out)) # modded-nanogpt introduced the use of relu^2
             mlp_out = jnp.einsum("bsE,Ee->bse", mlp_out, mlp.c_proj.astype(compute_dtype))
             x = x + mlp_out
             
         x = rms_norm(x)
         # note: we calculate logits and CE in fp32, so no weight downcasting here
+        # also perform logit softcapping
+        softcap = 15
         logits = jnp.einsum("bse,ev->bsv", x.astype(jnp.float32), self.lm_head)
+        logits = softcap * jax.nn.tanh(logits / softcap)
         return logits
 
 def estimate_flops(model: GPT):
@@ -216,7 +224,6 @@ def estimate_flops(model: GPT):
 
 
 def calculate_loss(idx: jax.Array, targets: jax.Array, model: GPT, dtype: jnp.dtype, reduce: bool=True) -> jax.Array:
-    # TODO try logit softcapping
     logits = model.forward(idx, dtype)
     # cross entropy loss using logsumexp
     logsumexp = jax.nn.logsumexp(logits, axis=-1)

nanojax/muon.py
@@ -10,36 +10,30 @@ from nanojax.gpt import GPT, Block
 
 @partial(
     register_dataclass,
-    data_fields=["adamw_mu", "adamw_nu", "mu"],# "wte_lr", "lm_head_lr", "lr"],
-    meta_fields=["b_1", "b_2", "eps", "adamw_wd", "wd", "ns_steps", "wte_lr", "lm_head_lr", "lr"]#, "warmup_ratio", "warmdown_ratio", "step", ]
+    data_fields=["adamw_mu", "adamw_nu", "mu"],
+    meta_fields=["b_1", "b_2", "eps", "wd", "ns_steps", "wte_lr", "lm_head_lr", "lr"]
 )
 @dataclass
 class Muon:
-    # Muon w/AdamW
-    # num_steps: int
+    ### Muon w/AdamW
     # Muon momentum states for out decoder blocks
     mu: list[Block]
     # AdamW states for embedding/lm_head respectively
     adamw_mu: tuple[jax.Array, jax.Array]
     adamw_nu: tuple[jax.Array, jax.Array]
 
-    # hyperparameters
+    # AdamW hyperparameters
     b_1: float = 0.9
     b_2: float = 0.95
     eps: float = 1e-10
-    adamw_wd: float = 0.0
-
-    ns_steps: int = 5 # number of newton-shulz iteration steps
     wd: float = 0.0
 
+    # number of newton-shulz iteration steps
+    ns_steps: int = 5 
     # we need seperate learning rates for our embeddings/lm_head/matrices
-    wte_lr: float = 3e-4
-    lm_head_lr: float = 3e-4
+    wte_lr: float = 0.2
+    lm_head_lr: float = 0.004
     lr: float = 0.02
-
-    # lr warmup parameters
-    # warmup_ratio: float = 0.0
-    # warmdown_ratio: float = 0.2
 
     @staticmethod              
     def init(model: GPT, **kwargs):
@@ -48,24 +42,45 @@ class Muon:
         mu = jax.tree.map(lambda p: p * 0.0, model.h)
 
         return Muon(mu=mu, adamw_mu=adamw_mu, adamw_nu=adamw_nu, **kwargs)
-        # return Muon(adamw_mu, adamw_nu, b_1, b_2, eps, adamw_wd, ns_steps, momentum, wte_lr, lm_head_lr, lr)
 
     def update(self, model: GPT, grads: GPT, lr_multiplier: float, step: int):
-        # update internal statistics based on gradients
-        ### adamw updates
-        adamw_mu = jax.tree.map(lambda m, g: m * self.b_1 + (1 - self.b_1) * g, self.adamw_mu, (grads.wte, grads.lm_head))
-        adamw_nu = jax.tree.map(lambda v, g: v * self.b_2 + (1 - self.b_2) * jax.lax.square(g), self.adamw_nu, (grads.wte, grads.lm_head))
-
-        # adamw bias updates
-        adamw_mu_ = jax.tree.map(lambda m: m / (1 - (self.b_1 ** step)), adamw_mu)
-        adamw_nu_ = jax.tree.map(lambda v: v / (1 - (self.b_2 ** step)), adamw_nu)
-        
-        adamw_update = jax.tree.map(lambda p, m, v, lr_: (lr_ * m / (jnp.sqrt(v) + self.eps)) - (lr_ * self.wd * p), (model.wte, model.lm_head), adamw_mu_, adamw_nu_, (self.wte_lr, self.lm_head_lr))
-
-        ### muon updates
+        """
+        This function applies Muon updates to all 2D matrices in our model, and AdamW to
+        our embedding and classifier layers.
+        Since we're returning a single update, u, to be subtracted from our model parameters,
+        some of the signage may look slightly different to other implementations.        
+        """
+        ### learning rate/momentum updates
         # momentum scheduler
         frac = jnp.minimum(step / 300, 1)
         momentum = (1 - frac) * 0.85 + frac * 0.95
+
+        # lr updates
+        wte_lr = self.wte_lr * lr_multiplier
+        lm_head_lr = self.lm_head_lr * lr_multiplier
+        lr = self.lr * lr_multiplier
+        
+        ### adamw updates
+        # first moment estimate
+        adamw_mu = jax.tree.map(lambda m, g: m * self.b_1 + (1 - self.b_1) * g, self.adamw_mu, (grads.wte, grads.lm_head))
+        # second moment estimate
+        adamw_nu = jax.tree.map(lambda v, g: v * self.b_2 + (1 - self.b_2) * jax.lax.square(g), self.adamw_nu, (grads.wte, grads.lm_head))
+
+        # bias corrected estimates
+        adamw_mu_ = jax.tree.map(lambda m: m / (1 - (self.b_1 ** step)), adamw_mu)
+        adamw_nu_ = jax.tree.map(lambda v: v / (1 - (self.b_2 ** step)), adamw_nu)
+
+        # parameter update with weight decay
+        adamw_update = jax.tree.map(
+            lambda p, m, v, lr_: (lr_ * m / (jnp.sqrt(v) + self.eps)) + (lr_ * self.wd * p),
+            (model.wte, model.lm_head), # p
+            adamw_mu_, # m
+            adamw_nu_, # v
+            (wte_lr, lm_head_lr) # lr_
+        )
+
+        ### muon updates
+        # mumoentum update
         mu = jax.tree.map(lambda m, g: m * momentum + (1 - momentum) * g, self.mu, grads.h)
         # nesterov update
         v = jax.tree.map(lambda m, g: g * (1 - momentum) + momentum * m, mu, grads.h)
@@ -89,7 +104,8 @@ class Muon:
             G = G.astype(jnp.float32)
             return G
 
-        muon_update = jax.tree.map(lambda u, g: newton_shulz(u, self.ns_steps) * (jnp.maximum(1, g.shape[-2] / g.shape[-1]))**0.5, v, grads.h)
+        # muon update - orthogonalize then scale 
+        muon_update = jax.tree.map(lambda u, g: lr * newton_shulz(u, self.ns_steps) * (jnp.maximum(1, g.shape[-2] / g.shape[-1]))**0.5, v, grads.h)
 
         updates = GPT(
             wte=adamw_update[0],
@@ -98,8 +114,7 @@ class Muon:
             cfg=model.cfg
         )
 
-        return updates, replace(self, mu=mu, adamw_mu=adamw_mu, adamw_nu=adamw_nu) #Muon(mu, adamw_mu, adamw_nu, self.b_1, self.b_2, self.eps, self.adamw_wd, self.ns_steps, self.momentum, self.wte_lr, self.lm_head_lr, self.lr)
-
-    
+        # we only need to update our optimizer states as the rest of the fields are static
+        return updates, replace(self, mu=mu, adamw_mu=adamw_mu, adamw_nu=adamw_nu)     
 
 

scripts/base_train.py
@@ -20,16 +20,23 @@ from nanojax.muon import Muon
 from nanojax.tokenizer import get_token_bytes, get_tokenizer
 
 config = configs.d3
+### optimization hparams
 lr = 3e-4
 batch_size = 32
 minibatch_size = 32
-seed = 42
 num_steps = -1
+warmup_ratio = 0.0
+warmdown_ratio = 0.2
+compute_dtype = jnp.float32
+
+### misc
+seed = 42
 accelerator_flops = 11.15e12 # 2080 super FLOPs/sec
+
+### training loop control
 profile_every = 100
 sample_every = 50
 eval_every = 50
-compute_dtype = jnp.float32
 
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))] + ["config", "compute_dtype"]
 exec(open(os.path.join('nanojax', 'configurator.py')).read()) # overrides from command line 
@@ -82,6 +89,18 @@ trackio.init(
     config=asdict(config)
 )
 
+def get_lr_multiplier(step):
+    warmup_iters = round(warmup_ratio * num_steps)
+    warmdown_iters = round(warmdown_ratio * num_steps)
+    if step < warmup_iters:
+        return (step + 1) / warmup_iters
+    elif step <= num_steps - warmdown_iters:
+        return 1.0
+    else:
+        progress = (num_steps - step) / warmdown_iters
+        return progress * 1.0 + (1 - progress) * final_lr_frac
+
+    
 @jax.jit
 def train_step(idx, targets, model, state):
     def inner_step(carry, j):
@@ -96,8 +115,8 @@ def train_step(idx, targets, model, state):
     grads = jax.tree.map(lambda g: g / grad_accm_steps, grads)
     loss /= grad_accm_steps
     
-    updates, state = state.update(model, grads, lr, step)
-    model = jax.tree.map(lambda p, u: p - u, model, updates)
+    updates, state = state.update(model, grads, lr_multiplier, step + 1)
+    model = jax.tree.map(jnp.subtract, model, updates)
     return model, state, loss
 
 prompts = [
@@ -109,10 +128,11 @@ prompts = [
 ]
 prompt_idx = [tokenizer.encode(p, prepend=tokenizer.get_bos_token_id()) for p in prompts]
 prompt_idx = [jnp.asarray(p, dtype=jnp.int32)[None, :] for p in prompt_idx]
-step = 1
-
 eval_forward = jax.jit(model.forward, static_argnums=1)
+
+step = 0
 while True:
+    lr_multiplier = get_lr_multiplier(step)
     d0 = time.perf_counter()
     model, state, loss = train_step(x, y, model, state)
     x, y = next(train_loader)

scripts/mid_train.py
@@ -131,7 +131,7 @@ def train_step(idx, targets, model, state):
 
     # step is accessed globally as it would trigger recompiles if passed to our JIT-ed step
     updates, state = state.update(model, grads, lr, step + 1)
-    model = jax.tree.map(lambda p, u: p - u, model, updates)
+    model = jax.tree.map(jnp.subtract, model, updates)
     return model, state, loss
 
 # this time we tokenizer prompts using our chat template