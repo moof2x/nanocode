fix accumulation precision error in cross entropy loss

nanojax/core_eval.py
@@ -169,10 +169,11 @@ def eval_forward(idx, masks, model, compute_dtype, ignore_idx, mesh):
     )
     def forward(idx, masks, model):
         logits, _ = model.forward(idx, compute_dtype=compute_dtype)
+        logits = logits.astype(jnp.float32)
         targets = jnp.roll(idx, -1, axis=1)
         target_masks = jnp.roll(masks, -1, axis=1)
 
-        logsumexp = jax.nn.logsumexp(logits.astype(jnp.float32), axis=-1)
+        logsumexp = jax.nn.logsumexp(logits, axis=-1)
         valid_targets = jnp.not_equal(targets, ignore_idx) & target_masks
         loss = -jnp.take_along_axis(logits, targets[:, :, None], axis=-1).squeeze(-1) + logsumexp
         loss = jnp.where(valid_targets, loss, 0.0)

nanojax/gpt.py
@@ -297,8 +297,9 @@ def estimate_flops(model: GPT) -> float:
 
 def calculate_loss(idx: jax.Array, targets: jax.Array, model: GPT, ignore_idx: int=-1,  compute_dtype: jnp.dtype=jnp.bfloat16, reduce: bool=True) -> jax.Array:
     logits, _ = model.forward(idx, compute_dtype=compute_dtype)
+    logits = logits.astype(jnp.float32)
     # cross entropy loss using logsumexp
-    logsumexp = jax.nn.logsumexp(logits.astype(jnp.float32), axis=-1)
+    logsumexp = jax.nn.logsumexp(logits, axis=-1)
     valid_targets = jnp.not_equal(targets, ignore_idx)
     loss = -jnp.take_along_axis(logits, targets[:, :, None], axis=-1).squeeze(-1) + logsumexp
     loss = jnp.where(valid_targets, loss, 0.0)

scripts/dpo.py
@@ -182,7 +182,8 @@ def concatenated_forward(idx, targets, model, ignore_idx: int=-1, compute_dtype:
     # our inputs and targets are comprise an equal number of chosen and rejected samples
     # obtain logprobs for all of these in one go
     logits, _ = model.forward(idx, compute_dtype=compute_dtype)
-    logp = jax.nn.log_softmax(logits.astype(jnp.float32), axis=-1) # bsv
+    logits = logits.astype(jnp.float32)
+    logp = jax.nn.log_softmax(logits, axis=-1) # bsv
     # grab logprobs for all our tokens
     per_token_logp = jnp.take_along_axis(logp, targets[:, :, None], axis=-1).squeeze(-1) # bsv -> bs
     # mask out padding tokens